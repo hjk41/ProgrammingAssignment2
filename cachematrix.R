f2c4351a44f41d6e7cc9935144e6a539696db823
Chuntao Hong <chuntao.hong@gmail.com>
2015-12-15 13:15:27 +0800

    add comment

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,8 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This is a inverse matrix solver that caches the inverse so
+## that we don't need to compute it over and over
 
-## Write a short comment describing this function
-
+## This function makes a wrapper over a matrix, so you can use
+## it on cacheSolver
 makeCacheMatrix <- function(x = matrix()) {
   inv <- NULL
   set <- function(y) {
@@ -18,10 +18,11 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## Write a short comment describing this function
-
+## This function takes a wrapped matrix produced by makeCacheMatrix
+## and computes the inverse of the matrix. It leverages caching
+## so if the inverse is already computed, we just return the
+## cached value
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
   inv <- x$getinv()
   if(!is.null(inv)) {
     message("getting cached data")
@@ -31,4 +32,19 @@ cacheSolve <- function(x, ...) {
   inv <- solve(data, ...)
   x$setinv(inv)
   inv
+}
+
+
+## testing the cache solver
+cacheSolveTest <- function() {
+  x = matrix(c(1,0,0,2), 2, 2)
+  message("x is: ")
+  print(x)
+  xx = makeCacheMatrix(x)
+  message("solving the first time: ")
+  y = cacheSolve(xx)
+  print(y)
+  message("solving the second time: ")
+  y = cacheSolve(xx)
+  print(y)
 }
